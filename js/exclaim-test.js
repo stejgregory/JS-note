88b5fb743c342692a669e75c812a65e0520790ce
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-11-07 15:56:05 +0100

    initial commit

---
+++ js/exclaim-test.js
@@ -0,0 +1,7 @@
+var exclaim = require("./js/exclaim").exclaim;
+
+if (exclaim("hi") !== "hi!!!!!" {
+        throw new Error("Exclaiming hi should equal hi!!!!!");
+    } else {
+        console.log(".");
+    };
---
+++ js/exclaim.js
@@ -0,0 +1,11 @@
+"use strict";
+
+(function(exports) {
+    var EXCLAMATION_MARK_COUNT = 5
+
+    function exclaim(string) {
+        return string + "!".repeat(EXCLAMATION_MARK_COUNT);
+    };
+
+    exports.exclaim = exclaim;
+})(this);
