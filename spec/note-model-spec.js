7fba4dcc248863e05897553a4a4d16f4325f03dd
andy jenkins <andygjenkins@gmail.com>
2016-11-08 10:01:03 +0000

    Add test-framework and test for .text property

--- spec/note-model-spec.js
+++ spec/note-model-spec.js
@@ -1,14 +1,6 @@
-"use strict";
+function testNoteText(text) {
+  var note = new Note(text);
+  assert.isTrue(note.text === text);
+}
 
-(function(exports) {
-    var note = new Note("test start text");
-
-    function testTextStoredInNote() {
-        if (note.returnText() !== "test start text") {
-            throw new Error("Text not stored correctly");
-        };
-    };
-
-    exports.testTextStoredInNote = testTextStoredInNote;
-
-})(this);
+testNoteText("I don't love javascript");
--- src/note-model.js
+++ src/note-model.js
@@ -1,9 +1,8 @@
-"use strict";
+(function(exports) {
 
 function Note(text) {
-    this.text = text
-};
+  this.text = text;
+}
 
-Note.prototype.returnText = function() {
-    return this.text
-};
+exports.Note = Note;
+})(this);
