f9e9b4b3f662cb1ad9fc02932f49f5588b5421a1
Stevie Winston-Gore <slwinstongore@gmail.com>
2016-11-08 09:20:34 +0000

    removed unnecessary code

--- spec/note-list-model-spec.js
+++ spec/note-list-model-spec.js
@@ -2,7 +2,7 @@ function testNoteListReturnsNotes() {
   var noteList = new NoteList();
 
   noteList.addNote("Hello Glasgae");
-  console.log(noteList.returnNotes()[0].returnText());
+  // console.log(noteList.returnNotes()[0].returnText());
 
   if (noteList.returnNotes()[0].returnText() !== "Hello Glasgae") {
     console.log("You arenae displaying 'e note pal")
