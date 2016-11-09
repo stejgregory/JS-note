84fd0ccebdf068f67c37743dab54d5c1e1cf7bd4
Stephen Gregory <stejgregory@gmail.com>
2016-11-09 16:49:11 +0000

    Stuck on step 8, failing on testNoteLessThanTwentyCharacters

--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -4,13 +4,29 @@ function testNoteListView() {
   var noteList = new NoteList();
   noteList.addNote("Hello Glasgae");
   noteList.addNote("Bye!");
+  noteList.addNote("This note is alot longer than all the other notes, it should be cut down to roughly 20 characters")
   var noteView = new NoteView(noteList);
 
-  if (noteView.html() !== "<ul><li><div>Hello Glasgae</div></li><li><div>Bye!</div></li></ul>") {
-    console.log("Error: " + noteView.html());
+  if (noteView.html(["Hello Glasgae", "Bye!", "This note is alot lo"]) !== "<ul><li><div>Hello Glasgae</div></li><li><div>Bye!</div></li><li><div>This note is alot lo</div></li></ul>") {
+    console.log("Error: testNoteListView");
   } else {
-    console.log("Passed: " + noteView.html());
+    console.log("Passed: testNoteListView");
+  }
+}
+
+function testNoteLessThanTwentyCharacters() {
+  var noteList = new NoteList();
+  noteList.addNote("This note is alot longer than all the other notes, it should be cut down to roughly 20 characters");
+
+  var noteView = new NoteView(noteList);
+
+  if (noteView.restrictLength() !== ["This note is alot l"]) {
+    console.log(noteView.restrictLength());
+    console.log("Fail");
+  } else {
+    console.log("Pass");
   }
 }
 
 testNoteListView();
+testNoteLessThanTwentyCharacters();
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -5,13 +5,23 @@
     this.notes = list.returnNotes();
   }
 
-  NoteView.prototype.html = function() {
+  NoteView.prototype.html = function(noteSubstring) {
     var htmlString = "<ul>";
-    for (var i = 0; i < this.notes.length; i++) {
-      htmlString += "<li><div>" + this.notes[i].returnText() + "</div></li>";
+    for (var i = 0; i < noteSubstring.length; i++) {
+      htmlString += "<li><div>" + noteSubstring[i] + "</div></li>";
     }
     htmlString += "</ul>";
     return htmlString;
+  }
+
+  NoteView.prototype.restrictLength = function() {
+    var noteSubstring = [];
+    for (var i = 0; i < this.notes.length; i++) {
+      var singleNote = (this.notes[i].returnText());
+      var singleNoteRestrict = singleNote.substr(0, 19);
+      noteSubstring.push(singleNoteRestrict);
+    }
+    return noteSubstring;
   }
 
   exports.NoteView = NoteView;
