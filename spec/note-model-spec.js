52125caa427197711ab332c1e4153ad1a3453a5e
Stephen Gregory <stejgregory@gmail.com>
2016-11-10 12:30:51 +0000

    Test written for unique note model ID

--- spec/note-model-spec.js
+++ spec/note-model-spec.js
@@ -1,13 +1,28 @@
 function testNoteText(text) {
-    var note = new Note(text);
-    assert.isTrue(note.text === text);
+  var note = new Note(text);
+  assert.isTrue(note.text === text);
+}
+
+function testGetText(text) {
+  var note = new Note(text);
+  assert.isTrue(note.getText() === text);
+}
+
+function testNoteHasID(text) {
+  var note = new Note(text);
+  actual = note.id;
+  expected = 1
+  assert.isTrue(actual===expected);
+}
+
+function testNoteIDIncrimentsUp() {
+  var noteOne = new Note("note1");
+  var noteTwo = new Note("note2");
+  actual = noteTwo.id;
+  expected = 2
+  assert.isTrue(actual===expected);
 }
 
 testNoteText("I don't love javascript");
-
-function testGetText(text) {
-    var note = new Note(text);
-    assert.isTrue(note.getText() === text);
-}
-
 testGetText("Hjelp meg vær så snill");
+testNoteIDIncrimentsUp();
